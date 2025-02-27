0c615e7278048844baa425a7f219ad7d298346cc
Abuakar <abubakars.siddik2146@gmail.com>
2025-02-27 18:14:03 +0000

    minor bug fixes to the api routes

---
+++ src/app/Modules/admin/Services/admin.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+
+  createQuizTest(apiRoute: string, formData: any) : Observable<any>{
+    const url = `/api/${apiRoute}`;
+    return this.http.post(url, formData);
+  }
+}
